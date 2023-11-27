92ebfd1ef04829b8a1f185f504dac51c250aa97a
ParthaI <partha@turbot.com>
2023-11-27 10:52:48 +0530

    Removed the columns for which the column value is always null

--- whois/table_rdap_domain.go
+++ whois/table_rdap_domain.go
@@ -25,7 +25,6 @@ func tableRdapDomain(ctx context.Context) *plugin.Table {
 			{Name: "domain_id", Type: proto.ColumnType_STRING, Transform: transform.FromField("Handle").NullIfZero(), Description: "Unique identifier for the domain."},
 			{Name: "object_class_name", Type: proto.ColumnType_STRING, Description: "String 'domain' representing the object type in RDAP."},
 			{Name: "ldh_name", Type: proto.ColumnType_STRING, Description: "Textual representation of DNS names.", Transform: transform.FromField("LDHName")},
-			{Name: "port43", Type: proto.ColumnType_STRING, Description: "Hostname of Registry WHOIS server."},
 
 			// JSON fields
 			{Name: "conformance", Type: proto.ColumnType_JSON, Description: "An array of strings, each providing a hint on the used specification."},
@@ -35,9 +34,7 @@ func tableRdapDomain(ctx context.Context) *plugin.Table {
 			{Name: "nameservers", Type: proto.ColumnType_JSON, Description: "An array of nameserver objects."},
 			{Name: "network", Type: proto.ColumnType_JSON, Description: "Information about IP address blocks and network allocations."},
 			{Name: "notices", Type: proto.ColumnType_JSON, Description: "Information about the service."},
-			{Name: "public_ids", Type: proto.ColumnType_JSON, Description: "Public identifiers associated with the domain.", Transform: transform.FromField("PublicIDs")},
 			{Name: "secure_dns", Type: proto.ColumnType_JSON, Description: "Secure DNS information.", Transform: transform.FromField("SecureDNS")},
-			{Name: "remarks", Type: proto.ColumnType_JSON, Description: "Additional remarks or notes associated with the domain registration."},
 			{Name: "status", Type: proto.ColumnType_JSON, Description: "An array of status flags describing the object state."},
 			{Name: "variants", Type: proto.ColumnType_JSON, Description: "The internationalized domain name (IDN) variants."},
 		},
